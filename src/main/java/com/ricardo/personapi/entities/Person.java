69dd307eb62858eebe5669085097ce89877b136f
Ricardo Martins <ricardomc.ifba@gmail.com>
2021-06-15 18:07:39 -0300

    Correction to entity person

--- src/main/java/com/ricardo/personapi/entities/Person.java
+++ src/main/java/com/ricardo/personapi/entities/Person.java
@@ -1,9 +1,10 @@
 package com.ricardo.personapi.entities;
 
 import java.time.LocalDate;
-
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -12,39 +13,33 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import org.hibernate.annotations.CascadeType;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
-@Entity
+@AllArgsConstructor
 public class Person {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@Column(nullable=false)
-	private String firstName;
-	
-	@Column(nullable=false)
-	private String lastName;
-	
-	@Column(nullable=false, unique = true)
-	private String cpf;
-	
-	private LocalDate birthDate;
-	
-	@Column(nullable=false)
-	@OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
-	private List<Phone> phones;
-	
-	
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    private LocalDate birthDate;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Phone> phones = new ArrayList<>();
 }
