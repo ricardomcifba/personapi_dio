3609534ba8b71d1bf1b9c1774fce37b0eb2ba7cd
Ricardo Martins <ricardomc.ifba@gmail.com>
2021-06-15 23:24:58 -0300

    Person Controller created

--- src/main/java/com/ricardo/personapi/controller/PersonController.java
+++ src/main/java/com/ricardo/personapi/controller/PersonController.java
@@ -1,17 +1,39 @@
 package com.ricardo.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ricardo.personapi.dto.response.MessageResponseDTO;
+import com.ricardo.personapi.entities.Person;
+import com.ricardo.personapi.repository.PersonRepository;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 	
-	@GetMapping
-	public String getBook() {
+	
+	private PersonRepository personRepository;
+	
+	@Autowired
+	public PersonController(PersonRepository personRepository) {
 		
-	return "Api Test!";
+		this.personRepository = personRepository;
+		
+	}
+	
+	
+	@PostMapping
+	public MessageResponseDTO createPerson(@RequestBody Person person) {
+		Person savedPerson = personRepository.save(person);
+		
+		return MessageResponseDTO
+				.builder()
+				.message("Created perso with ID " + savedPerson.getId())
+				.build();
+		
 	
 	}
 	
---
+++ src/main/java/com/ricardo/personapi/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.ricardo.personapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ricardo.personapi.entities.Person;
+
+public interface PersonRepository extends JpaRepository<Person, Long>{
+
+}
