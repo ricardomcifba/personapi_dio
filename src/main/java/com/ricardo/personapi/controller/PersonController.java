3ebcd1a5a2624bc26ad4a37a450f2c33d958affd
Ricardo Martins <ricardomc.ifba@gmail.com>
2021-06-16 16:23:08 -0300

    Person service created

--- src/main/java/com/ricardo/personapi/controller/PersonController.java
+++ src/main/java/com/ricardo/personapi/controller/PersonController.java
@@ -1,40 +1,38 @@
 package com.ricardo.personapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ricardo.personapi.dto.response.MessageResponseDTO;
 import com.ricardo.personapi.entities.Person;
-import com.ricardo.personapi.repository.PersonRepository;
+import com.ricardo.personapi.service.PersonService;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 	
 	
-	private PersonRepository personRepository;
+	private PersonService personService;;
 	
 	@Autowired
-	public PersonController(PersonRepository personRepository) {
+	public PersonController(PersonService personService) {
 		
-		this.personRepository = personRepository;
+		this.personService = personService;
 		
 	}
 	
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public MessageResponseDTO createPerson(@RequestBody Person person) {
-		Person savedPerson = personRepository.save(person);
 		
-		return MessageResponseDTO
-				.builder()
-				.message("Created perso with ID " + savedPerson.getId())
-				.build();
+		return personService.createPerson(person);	
 		
-	
 	}
 	
 }
